f78cc8f227542586218775aa5a366baba5b0d58d
higorrofino <higorrofino1@gmail.com>
2024-02-18 00:06:57 -0300

    committed

--- final-prject/calculator.js
+++ final-prject/calculator.js
@@ -1,3 +1,5 @@
+const inst = document.getElementById("instructions");
+const instructionsList = [];
 // sum
 function add(number1, number2) {
   return number1 + number2;
@@ -55,18 +57,33 @@ const divideNumbers = () => {
 
 document.querySelector("#divNumbers").addEventListener("click", divideNumbers);
 
+const displayInstructions = (instructions) => {
+  instructions.forEach((instruction) => {
+    // Create HTML elements
+    const paragraf = document.createElement("p");
+
+    // Set properties
+    paragraf.textContent = instruction.Operator + ":" + instruction.Description;
+
+    // Append elements
+    inst.appendChild(paragraf);
+  });
+};
+
 const instructions = async () => {
   try {
     const response = await fetch(
-      "https://higorrofino.github.io/cse121b/instructions.json"
+      "https://higorrofino.github.io/cse121b/final-prject/instructions.json"
     );
     if (!response.ok) {
       throw new Error("Failed to fetch temple data");
     }
-    const templeListData = await response.json();
-    templeList.push(...templeListData);
-    displayTemples(templeList);
+    const instructionsData = await response.json();
+    instructionsList.push(...instructionsData);
+    displayInstructions(instructionsList);
   } catch (error) {
     console.error("Error fetching temple data:", error);
   }
 };
+
+instructions();
