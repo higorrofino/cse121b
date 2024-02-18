b0ae5bfe7c686eaed53131888edf26b2e3106ebf
higorrofino <higorrofino1@gmail.com>
2024-02-17 23:42:07 -0300

    committed

--- final-prject/calculator.js
+++ final-prject/calculator.js
@@ -57,7 +57,9 @@ document.querySelector("#divNumbers").addEventListener("click", divideNumbers);
 
 const instructions = async () => {
   try {
-    const response = await fetch("");
+    const response = await fetch(
+      "https://higorrofino.github.io/cse121b/instructions.json"
+    );
     if (!response.ok) {
       throw new Error("Failed to fetch temple data");
     }
